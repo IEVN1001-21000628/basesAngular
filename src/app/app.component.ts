b2d68a4362e3a41fe6b7bc37505aa03c45548301
Daniel Ivan Guerrero Ponce <danielivanguerreroponce@gamil.com>
2024-09-17 21:05:14 -0600

    Primer commit martes 17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  /*template:`<div> hola mundo </div>
+  <h1> mi amor </h1>
+  <hr>
+  `,*/
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'basesAngular';
+
+  duplicarNumeros(x:number):number{
+    return x*2
+  }
+
+  alumno={
+    matricula: 1234,
+    nombre: 'Juan',
+    fechaInscrito: new Date(),
+    pago:2220
+  };
 }
